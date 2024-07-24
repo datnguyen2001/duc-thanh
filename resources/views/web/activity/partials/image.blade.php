f926a18983cc3f19e93e2abd56d283c1887cf09b
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2024-07-24 16:05:11 +0700

    update activity

--- resources/views/web/activity/partials/image.blade.php
+++ resources/views/web/activity/partials/image.blade.php
@@ -4,12 +4,11 @@
     <div class="list-image">
         <h5 class="sub-title">Hình ảnh</h5>
         <h1>
-            Hình ảnh trải nghiệm sản phẩm <br>
-            cùng Đức Thanh
+            Hình ảnh trải nghiệm sản phẩm cùng Đức Thanh
         </h1>
         <div class="swiper-container">
             <div class="list-product swiper-wrapper">
-                <div class="swiper-slide col-4">
+                <div class="swiper-slide">
                     <div class="product-image">
                         <img src="{{asset('assets/images/activity/frame-img.png')}}" alt="Frame" class="frame"/>
                         <img src="{{asset('assets/images/activity/image-product.webp')}}" alt="Image" class="image"/>
@@ -20,7 +19,7 @@
                         <span><a href="#">Chi tiết -></a></span>
                     </div>
                 </div>
-                <div class="swiper-slide col-4">
+                <div class="swiper-slide">
                     <div class="product-image">
                         <img src="{{asset('assets/images/activity/frame-img.png')}}" alt="Frame" class="frame"/>
                         <img src="{{asset('assets/images/activity/image-product.webp')}}" alt="Image" class="image"/>
@@ -31,7 +30,7 @@
                         <span><a href="#">Chi tiết -></a></span>
                     </div>
                 </div>
-                <div class="swiper-slide col-4">
+                <div class="swiper-slide">
                     <div class="product-image">
                         <img src="{{asset('assets/images/activity/frame-img.png')}}" alt="Frame" class="frame"/>
                         <img src="{{asset('assets/images/activity/image-product.webp')}}" alt="Image" class="image"/>
@@ -43,11 +42,9 @@
                     </div>
                 </div>
             </div>
-            <!-- Add Pagination -->
-            <div class="swiper-pagination"></div>
             <!-- Add Navigation -->
-            <div class="swiper-button-next"></div>
-            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"><img src="{{asset('assets/images/activity/next-btn.png')}}" alt="Next"/></div>
+            <div class="swiper-button-prev"><img src="{{asset('assets/images/activity/prev-btn.png')}}" alt="Prev"/></div>
         </div>
     </div>
 </div>
@@ -55,15 +52,11 @@
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         var swiper = new Swiper('.swiper-container', {
-            slidesPerView: 3,
+            slidesPerView: 1,
             spaceBetween: 30,
             navigation: {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
             },
             breakpoints: {
                 992: {
