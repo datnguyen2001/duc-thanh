e9df46d1247f476579f300905f9fc3b41c7afc57
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2024-07-25 17:15:32 +0700

    Update contact

--- resources/views/web/contact/index.blade.php
+++ resources/views/web/contact/index.blade.php
@@ -3,21 +3,19 @@
 
 @section('style_page')
     <link rel="stylesheet" href="{{ asset('assets/web/css/contact.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/web/css/product.css') }}">
 @stop
 {{--content of page--}}
 @section('content')
     <div class="contact-page">
-        <div class="contact-banner">
-            <div class="container">
-                <div class="contact-title">
-                    <h1>
-                        Liên hệ với Đức Thanh<br>
-                        để được hỗ trợ
-                    </h1>
-                    <img src="{{asset('assets/images/contact/underlined.png')}}" alt="Underlined" />
-                </div>
-            </div>
-
+        <div class="position-relative header-sp-desktop">
+            <img src="{{asset('assets/images/contact/banner-desktop.webp')}}" alt="" class="w-100 img-banner-sp">
+            <div class="title-banner-sp">Liên hệ với Đức Thanh để được hỗ trợ</div>
+            <img src="{{asset('assets/images/line-sp.webp')}}" alt="" class="img-line-sp">
+        </div>
+        <div class="position-relative header-sp-mobile">
+            <img src="{{asset('assets/images/contact/banner-mobile.webp')}}" alt="" class="w-100 img-banner-sp">
+            <div class="title-banner-sp">Liên hệ với Đức Thanh để được hỗ trợ</div>
         </div>
         <div class="home-contact">
             <div class="container">
