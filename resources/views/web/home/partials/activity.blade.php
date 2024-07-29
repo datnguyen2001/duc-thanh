aed45a738d624f10547539b845d5c94613d02f0a
Duc Truong <146322141+hiepduc24089@users.noreply.github.com>
2024-07-29 22:04:51 +0700

    update css

--- resources/views/web/home/partials/activity.blade.php
+++ resources/views/web/home/partials/activity.blade.php
@@ -18,14 +18,14 @@
         <div class="acts-image col-lg-8 col-12">
             <div class="frame-container">
                 <img src="{{asset('assets/images/home/frame1.png')}}" alt="Frame1" class="frame-1">
-                <img src="{{$imageProduct->src}}" alt="Image Activity" class="home-activity-img">
+                <img src="{{$imageProduct->src ?? ''}}" alt="Image Activity" class="home-activity-img">
             </div>
             <h5>{{__('home.activity_image')}}</h5>
         </div>
         <div class="acts-video col-lg-4 col-12">
             <div class="frame-container">
                 <img src="{{asset('assets/images/home/frame2.png')}}" alt="Frame2" class="frame-2">
-                <img src="{{$videoProduct->src}}" alt="Tiktok Activity" class="home-activity-video">
+                <img src="{{$videoProduct->src ?? ''}}" alt="Tiktok Activity" class="home-activity-video">
             </div>
             <h5>{{__('home.activity_video')}}</h5>
         </div>
