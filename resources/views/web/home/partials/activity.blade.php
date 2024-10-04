88addaf237540b75a7d15fe54959e0e9f9987d3c
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2024-10-04 10:09:09 +0700

    update route activity

--- resources/views/web/home/partials/activity.blade.php
+++ resources/views/web/home/partials/activity.blade.php
@@ -22,14 +22,14 @@
                 <img src="{{asset('assets/images/home/frame1-mobile.png')}}" alt="Frame1" class="frame-1 frame-1-mobile">
 
             </div>
-            <h5>{{__('home.activity_image')}}</h5>
+            <a href="{{route('activity') . '#image-field'}}"><h5>{{__('home.activity_image')}}</h5></a>
         </div>
         <div class="acts-video col-lg-4 col-12" data-aos="fade-up">
             <div class="frame-container">
                 <img src="{{asset('assets/images/home/frame2.png')}}" alt="Frame2" class="frame-2">
                 <img src="{{$videoProduct->src ?? ''}}" alt="Tiktok Activity" class="home-activity-video">
             </div>
-            <h5>{{__('home.activity_video')}}</h5>
+            <a href="{{route('activity') . '#video-field'}}"><h5>{{__('home.activity_video')}}</h5></a>
         </div>
     </div>
 </div>
